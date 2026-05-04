fc2bcafc2f64f53f0b73d6c92389cd2b9cd9a09e
nmujahidin-aa <nurmujahidin18@gmail.com>
2026-05-04 21:10:26 +0700

    fix: delete package ue3-easy-data-table

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/main.css'
-import 'vue3-easy-data-table/dist/style.css'
+// import 'vue3-easy-data-table/dist/style.css'
 import './assets/styles/fix-easy-table.css'
 
 import { createApp } from 'vue'
